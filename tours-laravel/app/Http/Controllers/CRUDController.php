63fe9e964f8906135e077a1d1c76c81995ae0203
Huy Dang <giahuy.dng@gmail.com>
2021-12-18 10:42:51 +0700

    create crud controller and api

---
+++ tours-laravel/app/Http/Controllers/CRUDController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tours;
+use App\Models\TypeTours;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class CRUDController extends Controller
+{
+    public function index()
+    {
+        $tours = Tours::all();
+        return response()->json([
+            'status' => 200,
+            'tours' => $tours,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:100',
+//            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+            'image' => 'required',
+            'time' => 'required',
+            'price' => 'required',
+            'id_type' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'validate_err' => $validator->messages(),
+            ]);
+        } else {
+            $tour = new Tours;
+            $tour->name = $request->input('name');
+//            $image = $request->file('image');
+//            $filename =  $image->getClientOriginalExtension();
+//            $image->move('upload/', $filename);
+//            $tour->image = $filename;
+            $tour->image = $request->input('image');
+            $tour->time = $request->input('time');
+            $tour->price = $request->input('price');
+            $tour->price = $request->input('id_type');
+
+            $tour->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Tour Added Successfully',
+            ]);
+        }
+
+    }
+
+    public function edit($id)
+    {
+        //Return edit value view
+        $tour = Tours::find($id);
+        if ($tour) {
+            return response()->json([
+                'status' => 200,
+                'tour' => $tour,
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Tour ID Found',
+            ]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:100',
+            'image' => 'required',
+            'time' => 'required',
+            'price' => 'required',
+            'id_type' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'validationErrors' => $validator->messages(),
+            ]);
+        } else {
+            $tour = Tours::find($id);
+            if ($tour) {
+
+                $tour->name = $request->input('name');
+                $tour->image = $request->input('image');
+                $tour->time = $request->input('time');
+                $tour->price = $request->input('price');
+                $tour->price = $request->input('id_type');
+                $tour->update();
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Tour Updated Successfully',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No Tour ID Found',
+                ]);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $tour = Tours::find($id);
+        if ($tour) {
+            $tour->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Tour Deleted Successfully',
+            ]);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Tour ID Found',
+            ]);
+        }
+    }
+}
--- tours-laravel/routes/api.php
+++ tours-laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CRUDController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//start crud route
+Route::get('/view-tours', [CRUDController::class, 'index']);
+Route::get('/tours', [CRUDController::class, 'index']);
+Route::post('/add-tour', [CRUDController::class, 'store']);
+Route::get('/edit-tour/{id}', [CRUDController::class, 'edit']);
+Route::put('/update-tour/{id}', [CRUDController::class, 'update']);
+Route::delete('/delete-tour/{id}', [CRUDController::class, 'destroy']);
+//end crud route
